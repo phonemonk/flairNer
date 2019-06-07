dac7f2e47be2d86cb275342a4c02972dadc7e470
Vimal <vimal@Vimals-MacBook-Pro-4.local>
2019-06-07 18:38:39 +0530

    Base docker image for the flair library. Code to integrate with Acharya in progress.

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+From python:3.6-slim
+#From anibali/pytorch:no-cuda
+
+RUN apt-get update && apt-get install -y unixodbc-dev gcc g++
+
+RUN mkdir -p flairNER/
+COPY * flairNER/
+WORKDIR flairNER/
+
+RUN pip install -r requirements.txt
+
+#RUN python -c 'import flair; _ = flair.models.SequenceTagger.load("ner-fast")'
+
+CMD /bin/sh
---
+++ flairNer.py
@@ -0,0 +1,22 @@
+#!/bin/python
+
+#
+# Flair NER: Named Entity recognition using Flair library by Zalando Research.
+# Algo module created for Acharya by Vimal Menon
+# 2019
+#
+
+from flair.data import Corpus
+from flair.datasets import ColumnCorpus
+from flair.embeddings import TokenEmbeddings, WordEmbeddings, StackedEmbeddings
+from typing import List
+
+class ner_trainer(object):
+    def __init__(self): 
+        self.dataFolder = dataFolder
+        self.trainFile = trainFaile
+        self.columns = {0: 'text', 1: 'pos', 2: 'ner'}
+        pass
+
+    def LoadConll03(self, dataFolder, trainFile, testFile=None, devFile=None):
+        self.corpus = ColumnCorpus(dataFolder, self.columns, trainFile, testFile, devFile) 
