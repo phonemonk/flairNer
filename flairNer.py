c2bacff83a4ab8fbc320b143773e42e068cec9fb
Vimal <vimal@Vimals-MacBook-Pro-4.local>
2019-06-22 13:50:15 +0530

    added further code for training NER

--- flairNer.py
+++ flairNer.py
@@ -9,10 +9,13 @@
 from flair.data import Corpus
 from flair.datasets import ColumnCorpus
 from flair.embeddings import TokenEmbeddings, WordEmbeddings, StackedEmbeddings
+from flair.models import SequenceTagger
+from flair.trainers import ModelTrainer
 from typing import List
 
 class ner_trainer(object):
     def __init__(self, dataFolder, trainFile): 
+        self.tag_type = 'ner'
         self.dataFolder = dataFolder
         self.trainFile = trainFile
         self.columns = {0: 'text', 1: 'pos', 2: 'empty', 3: 'ner'}
@@ -20,4 +23,29 @@ class ner_trainer(object):
 
     def LoadConll03(self, dataFolder, trainFile, testFile=None, devFile=None):
         self.corpus = ColumnCorpus(dataFolder, self.columns, trainFile, testFile, devFile) 
-        print(self.corpus.train[0].to_tagged_string('ner'))
+        self.tag_dictionary = self.corpus.make_tag_dictionary(tag_type=self.tag_type)
+        #print(self.corpus.train[0].to_tagged_string('ner'))
+        print(self.tag_dictionary.idx2item)
+
+    def LoadEmbeddings(self, emedding_types):
+        self.embedding_types = emedding_types
+        self.embeddings = StackedEmbeddings(embeddings=self.embedding_types)
+
+    def train(self, model_path, 
+            learning_rate=0.1, 
+            batch_size=32, 
+            epochs=150, 
+            hidden_size=256, 
+            use_crf=True):
+        self.tagger = SequenceTagger(hidden_size=hidden_size,
+                                    embeddings=self.embeddings,
+                                    tag_dictionary=self.tag_dictionary,
+                                    tag_type=self.tag_type,
+                                    use_crf=use_crf)
+
+        self.trainer = ModelTrainer(self.tagger, self.corpus)
+
+        self.trainer.train(model_path, 
+                            learning_rate=learning_rate, 
+                            mini_batch_size=batch_size, 
+                            max_epochs=epochs)
--- train_flair.py
+++ train_flair.py
@@ -3,6 +3,7 @@ import sys
 import os
 import logging
 
+from flair.embeddings import TokenEmbeddings, WordEmbeddings, StackedEmbeddings
 from flairNer import ner_trainer
 
 logpath = "train_ner.log"
@@ -20,6 +21,12 @@ datafolder = "data"
 nt = ner_trainer(datafolder, infile)
 
 nt.LoadConll03(datafolder, infile)
+
+nt.LoadEmbeddings([WordEmbeddings('glove')])
+
+nt.train("models/tagger1")
+
+print("Done")
 
 
 
